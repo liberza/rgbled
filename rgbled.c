039dc5ed4cb9e55ccc5c3c38f6e8d66b9be5c9c4
Nick Levesque <nicholas.a.levesque@maine.edu>
2015-04-04 17:05:06 -0400

    added rgbled_init()

--- rgbled.c
+++ rgbled.c
@@ -7,33 +7,64 @@
 #include <linux/cdev.h>
 #include <linux/mutex.h>
 #include <asm/uaccess.h>
+#include <linux/gpio.h>
 
 #define DRIVER_AUTHOR	"Nick Levesque <nick.levesque@gmail.com>
 #define DRIVER_DESC	"Sets red, green and blue values for external LED"
 #define DEVICE_NAME	"rgbled"
 
 struct rgb_dev {
-	struct mutex lock;
 }
 
 // Implementation of file operation methods
 
 int rgb_open(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "rgb: opened device\n");
+	#ifdef DEBUG
+	printk(KERN_INFO "rgbled: opened device\n");
+	#endif
 	return 0;
 }
 
 // Allow threads to read
 int rgb_read(struct file *filp, char *buf, size_t buf_cnt, loff_t* offset)
 {
-	printk(KERN_INFO "rgb, read from device\n");
+	#ifdef DEBUG
+	printk(KERN_INFO "rgbled read from device\n");
+	#endif
 	return 0;
 }
 
 // Allow threads to write colors to LED
 ssize_t rgb_write(struct file *filp, const char *src_buf, size_t buf_cnt, loff_t* offset)
 {
-	// Wait for lock
-	if (mutex_lock_interruptible(&stintdev.lock) {
-		
+	#ifdef DEBUG
+	printk(KERN_INFO "rgbled: write to device\n");
+	#endif
+	return 0;
+}
+
+static int __init rgbled_init(void)
+{
+	rgbled.ret = alloc_chrdev_region(&rgbleddev.dev_num, 0, 1, DEVICE_NAME);
+	if (rgbleddev.ret < 0) {
+		prink(KERN_ALERT "rgbled: allocating major number failed\n");
+		return rgbleddev.ret;
+	}
+	rgbleddev.major_num = MAJOTR(rgbleddev.dev_num);
+	#ifdef DEBUG
+	printk(KERN_INFO "rgbled: major num = %d\n", rgbleddev.major_num);
+	#endif
+
+	rgbleddev.cdev = cdev_alloc();
+	rgbleddev.cdev->ops = &fops;
+	rgbleddev.cdev->owner = THIS_MODULE;
+	rgbleddev.ret = cdev_add(rgbleddev.cdev, rgbleddev.dev_num, 1);
+	if (rgbleddev.ret < 0) {
+		printk(KERN_ALERT "rgbled: failed to add cdev\n");
+		return rgbleddev.ret;
+	}
+	// lock init
+	rgbleddev.dev_num = MKDEV(rgbleddev.major_num, 0);
+	return 0;
+}
