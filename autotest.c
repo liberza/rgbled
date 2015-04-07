cc174136bed6b994f7d595e6192b5984c9d45d2c
nicholas.a.levesque <nicholas.a.levesque@maine.edu>
2015-04-07 04:34:55 +0000

    fix autotest logic

--- autotest.c
+++ autotest.c
@@ -29,8 +29,8 @@ int main(int argc, char *argv[])
 	// Tests:
 	// 1
 	errno = 0;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("1: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("1: pass\n");
 		else printf("1: fail\n");
 	}
 	// 2
@@ -38,8 +38,8 @@ int main(int argc, char *argv[])
 	c.red = -1;
 	c.green = -1;
 	c.blue = -1;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("2: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("2: pass\n");
 		else printf("2: fail\n");
 	}
 	// 3
@@ -47,8 +47,8 @@ int main(int argc, char *argv[])
 	c.red = -1;
 	c.green = 0;
 	c.blue = 0;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("3: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("3: pass\n");
 		else printf("3: fail\n");
 	}
 	// 4
@@ -56,8 +56,8 @@ int main(int argc, char *argv[])
 	c.red = 0;
 	c.green = -1;
 	c.blue = 0;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("4: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("4: pass\n");
 		else printf("4: fail\n");
 	}
 	// 5
@@ -65,8 +65,8 @@ int main(int argc, char *argv[])
 	c.red = 0;
 	c.green = 0;
 	c.blue = -1;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("5: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("5: pass\n");
 		else printf("5: fail\n");
 	}
 	// 6
@@ -74,8 +74,8 @@ int main(int argc, char *argv[])
 	c.red = 2048;
 	c.green = 2048;
 	c.blue = 2048;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("6: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("6: pass\n");
 		else printf("6: fail\n");
 	}
 
@@ -84,8 +84,8 @@ int main(int argc, char *argv[])
 	c.red = 2048;
 	c.green = 2047;
 	c.blue = 2047;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("7: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("7: pass\n");
 		else printf("7: fail\n");
 	}
 	// 8
@@ -93,8 +93,8 @@ int main(int argc, char *argv[])
 	c.red = 2047;
 	c.green = 2048;
 	c.blue = 2047;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("8: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("8: pass\n");
 		else printf("8: fail\n");
 	}
 
@@ -103,8 +103,8 @@ int main(int argc, char *argv[])
 	c.red = 2047;
 	c.green = 2047;
 	c.blue = 2048;
-	if (ioctl(fh, RGB_SET, &c) == -1) {
-		if (errno == -EINVAL) printf("9: pass\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) {
+		if (errno == EINVAL) printf("9: pass\n");
 		else printf("9: fail\n");
 	}
 
@@ -113,25 +113,25 @@ int main(int argc, char *argv[])
 	c.red = 2047;
 	c.green = 2047;
 	c.blue = 2047;
-	if (ioctl(fh, RGB_SET_RW, &c) == -1) {
-		if (errno == -ENOTSUP) printf("10: pass\n");
+	if (ioctl(fh, RGB_SET_RW, &c) < 0) {
+		if (errno == ENOTSUP) printf("10: pass\n");
 		else printf("10: fail\n");
 	}
 
 	// 11
 	errno = 0;
-	if (ioctl(fh, RGB_READ, &c) == -1) {
-		if (errno == -ENOTSUP) printf("11: pass\n");
+	if (ioctl(fh, RGB_READ, &c) < 0) {
+		if (errno == ENOTSUP) printf("11: pass\n");
 		else printf("11: fail\n");
 	}
 	
 	// 12
-	if ((write(fh, NULL, 0)) == -ENOTSUP) printf("12: pass\n");
+	if ((write(fh, NULL, 0)) == ENOTSUP) printf("12: pass\n");
 	else printf("12: fail\n");
 	
 	
 	// 13
-	if ((read(fh, NULL, 0)) == -ENOTSUP) printf("13: pass\n");
+	if ((read(fh, NULL, 0)) == ENOTSUP) printf("13: pass\n");
 	else printf("13: fail\n");
 	
 	
@@ -139,7 +139,7 @@ int main(int argc, char *argv[])
 	c.red = 0;
 	c.green = 0;
 	c.blue = 0;
-	if (ioctl(fh, RGB_SET, &c) == -1) printf("14: fail\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) printf("14: fail\n");
 	else printf("14: pass (LED off)\n");
 	printf("Press enter to continue\n");
 	getchar();
@@ -149,7 +149,7 @@ int main(int argc, char *argv[])
 	c.red = 2047;
 	c.green = 2047;
 	c.blue = 2047;
-	if (ioctl(fh, RGB_SET, &c) == -1) printf("15: fail\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) printf("15: fail\n");
 	else printf("15: pass (LED white)\n");
 	printf("Press enter to continue\n");
 	getchar();
@@ -159,7 +159,7 @@ int main(int argc, char *argv[])
 	c.red = 2047;
 	c.green = 0;
 	c.blue = 0;
-	if (ioctl(fh, RGB_SET, &c) == -1) printf("16: fail\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) printf("16: fail\n");
 	else printf("16: pass (LED red)\n");
 	printf("Press enter to continue\n");
 	getchar();
@@ -169,7 +169,7 @@ int main(int argc, char *argv[])
 	c.red = 0;
 	c.green = 2047;
 	c.blue = 0;
-	if (ioctl(fh, RGB_SET, &c) == -1) printf("17: fail\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) printf("17: fail\n");
 	else printf("17: pass (LED green)\n");
 	printf("Press enter to continue\n");
 	getchar();
@@ -179,7 +179,7 @@ int main(int argc, char *argv[])
 	c.red = 0;
 	c.green = 0;
 	c.blue = 2047;
-	if (ioctl(fh, RGB_SET, &c) == -1) printf("18: fail\n");
+	if (ioctl(fh, RGB_SET, &c) < 0) printf("18: fail\n");
 	else printf("18: pass (LED blue)\n");
 	printf("Press enter to continue\n");
 	getchar();
@@ -189,21 +189,21 @@ int main(int argc, char *argv[])
 	#if 0
 	for(ever) {
 		while (c.red > 0) {
-			if (ioctl(fh, RGB_SET, &c) == -1)
+			if (ioctl(fh, RGB_SET, &c) < 0)
 				perror("ioctl: ");
 			usleep(delay);
 			c.red--;
 			c.green++;
 		}
 		while (c.green > 0) {
-			if (ioctl(fh, RGB_SET, &c) == -1)
+			if (ioctl(fh, RGB_SET, &c) < 0)
 				perror("ioctl: ");
 			usleep(delay);
 			c.green--;
 			c.blue++;
 		}
 		while (c.blue > 0) {
-			if (ioctl(fh, RGB_SET, &c) == -1)
+			if (ioctl(fh, RGB_SET, &c) < 0)
 				perror("ioctl: ");
 			usleep(delay);
 			c.blue--;
