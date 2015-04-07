96653f69839d4cf6ee1726a1a8756a1492f45136
Nick Levesque <nick.levesque@gmail.com>
2015-04-07 08:54:16 -0400

    testing threadtest.pl

--- threadtest.pl
+++ threadtest.pl
@@ -1,6 +1,31 @@
 #!/usr/bin/perl
-$pid = fork();
-if (defined $pid && $pid == 0) {
-	system("./client " . $red . " " . $green . " " . $blue);
-	exit 0;
+$green = 0;
+$blue = 2047;
+while ($red > 0) {
+	$pid = fork();
+	if (defined $pid && $pid == 0) {
+	#	system("./client " . $red . " " . $green . " " . $blue);
+		printf("$red, $green, $blue");
+		exit 0;
+	}
+	$red--;
+	$blue++;
+}
+while ($blue > 0) {
+	$pid = fork();
+	if (defined $pid && $pid == 0) {
+		printf("$red, $green, $blue\n");
+		exit 0;
+	}
+	$blue--;
+	$green++;
+}
+while ($green > 0) {
+	$pid = fork();
+	if (defined $pid && $pid == 0) {
+		printf("$red, $green, $blue\n");
+		exit 0;
+	}
+	$green--;
+	$red++;
 }
